233617a9744167ee70c4c655a2bfb4ebec9783fc
Arunava-27 <drarunkundu22@gmail.com>
2026-04-07 14:02:39 +0530

    Add .gitignore and initial demo1.cpp file

---
+++ demo1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x and y are : " << x << " and " << y << endl;
+
+    return 0;
+}
